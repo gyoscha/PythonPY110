a345146cdcd3e115b28955ca6deba72ee10355f1
geosok <sokolovgeorgy@gmail.com>
2021-11-10 20:35:27 +0300

    Практика 10.11.2021

--- Занятие1/Домашние_задания/task1_1/main.py
+++ Занятие1/Домашние_задания/task1_1/main.py
@@ -1,3 +1,11 @@
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    from random import randint
+
+    print('Список продуктов:')
+
+    price = [randint(51, 199) for value in range(20)]
+    product = ['яйца', 'колбаса', 'хлеб', 'сыр']
+
+    for i, (price, product) in enumerate(zip(price, product), start=1):
+        print(f'№ {i}: - {product}: {price} руб.')
--- Занятие1/Лабораторные_задания/task4_1/main.py
+++ Занятие1/Лабораторные_задания/task4_1/main.py
@@ -4,7 +4,8 @@ from itertools import count
 def task():
     counter = count(100, 10)
 
-    # TODO распечатать с столбик первые 10 чисел бесконечного итератора
+    for _ in range(10):   # TODO распечатать с столбик первые 10 чисел бесконечного итератора
+        print(next(counter))
 
 
 if __name__ == "__main__":
