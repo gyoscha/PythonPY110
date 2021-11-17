c6bfa5ba55f23ab3009b6aa3b47d1b1efd1ce386
geosok <sokolovgeorgy@gmail.com>
2021-11-17 21:15:23 +0300

    Практика 17.11.2021

--- Занятие2/Домашние_задания/task1/main.py
+++ Занятие2/Домашние_задания/task1/main.py
@@ -1,5 +1,10 @@
+def count(start_number, geom):
+    while True:
+        yield start_number
+        start_number *= geom
 
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    my_count = count(6, 2)
+    for _ in range(20):
+        print(next(my_count))
--- Занятие2/Домашние_задания/task1_5/main.py
+++ Занятие2/Домашние_задания/task1_5/main.py
@@ -1,7 +1,12 @@
 from string import ascii_lowercase, ascii_uppercase, digits
+import random
 
 
 if __name__ == "__main__":
     print(ascii_lowercase)
     print(ascii_uppercase)
     print(digits)
+
+    alphabet = ascii_lowercase + ascii_uppercase + digits
+    password = ''.join(random.sample(alphabet, 8))
+    print(f'Рекомендуемый пароль: {password}')
