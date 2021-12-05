51027263a5d87690c7ffde097ad4de2052dcb7e7
gyoscha <sokolovgeorgy@gmail.com>
2021-12-05 16:22:36 +0300

    ДЗ 05.12.2021

--- Занятие4/Домашние_задания/Доп. задание 1.py
+++ Занятие4/Домашние_задания/Доп. задание 1.py
@@ -11,3 +11,32 @@
 
 
 # ToDo Сделал на сайте Regex (https://regex101.com/r/2ZwBZ5/1). Закончить в Python
+import re
+
+
+if __name__ == '__main__':
+    string_ = '1 + 10 = 11,' \
+              '20 - 12 = 8,' \
+              '7.5 / 2.5 = 3,' \
+              '3 * 0.5 = 1.5,' \
+              '0.05 + 6 = 6.05'
+
+    number_regex = r'\b(?P<first_number>\d+\.?\d?\d?)\s+(?P<sign>\+|\-|\*|\/)\s+(?P<second_number>\d+\.?\d?\d?)\s+\=\s+(?P<last_number>\d+\.?\d?\d?)'
+
+    pattern = re.compile(number_regex)
+
+    for item in pattern.finditer(string_):
+        print(item.groups())
+        print(bool(item))
+        if (item['sign'] == '+') and (float(item['first_number']) + float(item['second_number']) == float(item['last_number'])):
+            print(f'Выражение: {item.group()} - верное')
+            print('-' * 10)
+        if (item['sign'] == '-') and (float(item['first_number']) - float(item['second_number']) == float(item['last_number'])):
+            print(f'Выражение: {item.group()} - верное')
+            print('-' * 10)
+        if (item['sign'] == '/') and (float(item['first_number']) / float(item['second_number']) == float(item['last_number'])):
+            print(f'Выражение: {item.group()} - верное')
+            print('-' * 10)
+        if (item['sign'] == '*') and (float(item['first_number']) * float(item['second_number']) == float(item['last_number'])):
+            print(f'Выражение: {item.group()} - верное')
+            print('-' * 10)
